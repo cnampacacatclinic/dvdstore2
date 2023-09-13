ce78bc374b27ec69987c416e9ebf59c2b8520285
Catherine Jules <catherine-jules@outlook.com>
2023-09-13 15:16:28 +0200

    correction d'un bug avec le post

--- dvdstore/src/main/java/com/simplon/dvdstore/dvdstore/controllers/DvdStoreController.java
+++ dvdstore/src/main/java/com/simplon/dvdstore/dvdstore/controllers/DvdStoreController.java
@@ -1,6 +1,5 @@
 package com.simplon.dvdstore.dvdstore.controllers;
 
-import com.simplon.dvdstore.dvdstore.services.DvdModelService;
 import org.springframework.web.bind.annotation.*;
 import com.simplon.dvdstore.dvdstore.repositories.DvdStoreRepositoryModel;
 import com.simplon.dvdstore.dvdstore.services.DvdStoreService;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,14 +19,14 @@ public class DvdStoreController {
     @Autowired
     private DvdStoreService dvdStoreService;
 
-    //Dans le navigateur http://localhost:8080/home
+    //Dans le navigateur http://localhost:8080/dvds/home
     /*@GetMapping("/home")
     public ArrayList<DvdModelService> getAllDvds() {
         return dvdStoreService.findAll();
     }/**/
 
     @GetMapping("/home")
-    public List<DvdStoreDto> getAllDvds() {
+    public ArrayList<DvdStoreDto> getAllDvds() {
         return dvdStoreService.findAll();
     }
 
