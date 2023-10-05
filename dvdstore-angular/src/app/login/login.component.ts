5c59f6844cef7b843c9e6ccdb190ac85419e26fc
catherinejj <catherine-jules@outlook.com>
2023-10-05 13:08:42 +0200

    feat : session for the role

--- dvdstore-angular/src/app/login/login.component.ts
+++ dvdstore-angular/src/app/login/login.component.ts
@@ -19,16 +19,19 @@ export class LoginComponent {
 
   async  formConnexion(formData: NgForm) {
     //on affiche dans la console le contenu du formulaire
-      console.log(formData.value);
-      //on envoie le contenu à la methode connexion dans le service
-      this.dvdService.connexion(formData.value);
-      //on recupére la reponse de la requete avec la methode reponseConnexion dans le service
-      const data = await this.dvdService.reponseConnexion(formData.value);
-      //on affiche dans la console la reponse
-      console.log(data);
-      //on range le token dans une session
-      sessionStorage.setItem("token",data.token)
-      alert('Success');
+    console.log(formData.value);
+    //on envoie le contenu à la methode connexion dans le service
+    this.dvdService.connexion(formData.value);
+    //on recupére la reponse de la requete avec la methode reponseConnexion dans le service
+    const data = await this.dvdService.reponseConnexion(formData.value);
+    //on affiche dans la console la reponse
+    console.log(data);
+    //on range le token dans une session
+    sessionStorage.setItem("token",data.token)
+    console.log(data.user.roles[0].name);
+    const userRole = data.user.roles[0].name;
+    sessionStorage.setItem("role",userRole);
+    alert('Success');
   }
 
 }
